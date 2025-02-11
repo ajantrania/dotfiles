c22afca92cb3ac043684a978ad5763e12247af2b
Apurva Jantrania <apurvaja@amazon.com>
2025-02-11 09:34:31 -0800

    Sketchybar: Update Colors

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,84 +1,44 @@
 #!/usr/bin/env sh
 
-# Color Palette -- Catppuccin Macchiato
-export BLACK=0xff181926 #181926
-export WHITE=0xffcad3f5 #cad3f5
-export RED=0xffed8796 #ed8796
-# export GREEN=0xffa6da95 #a6da95
-# export BLUE=0xff8aadf4 #8aadf4
-# export YELLOW=0xffeed49f # eed49f
-# export ORANGE=0xfff5a97f #f5a97f
-# export MAGENTA=0xffc6a0f6 #c6a0f6
-# export GREY=0xff939ab7 #939ab7
-# export SKY=0xff91d7e3 #91d7e3
-# export PINK=0xfff5bde6 #f5bde6
-# export TRANSPARENT=0x00000000
+# Primary Colors
+export ORANGE=0xFFFA8F69          #0xFF9164
 
-# export ROSEWATER=0xFFf5e0dc #f5e0dc
-# export FLAMINGO=0xFFf2cdcd #f2cdcd
-# export PINK=0xFFf5c2e7 #f5c2e7
-# export MAUVE=0xFFcba6f7 #cba6f7
-# export RED=0xFFf38ba8 #f38ba8
-# export MAROON=0xFFeba0ac #eba0ac
-export PEACH=0xFFfab387 #fab387
-# export YELLOW=0xFFf9e2af #f9e2af
-# export GREEN=0xFFa6e3a1 #a6e3a1
-# export TEAL=0xFF94e2d5 #94e2d5
-# export SKY=0xFF89dceb #89dceb
-# export SAPPHIRE=0xFF74c7ec #74c7ec
-# export BLUE=0xFF89b4fa #89b4fa
-# export LAVENDER=0xFFb4befe #b4befe
-export NAVY=0xFF1E66F5 #1E66F5
+export NAVY_BLUE=0xFF2B4D7E       #0x2B4D7E
+export MIDNIGHT_BLUE=0xFF13294B   #0x13294B
 
-# export WHITE=0xFFcdd6f4 #cdd6f4
-# export DARK_WHITE=0xFF9399b2  #9399b2
-export DARK_WHITE_SEMI_TRANSPARENT=0x999399b2  #9399b2
-export BRIGHT_WHITE=0xFFE8ECFC #e8ecfc
+# Supporting Colors
+export WHITE=0xFFE1E3E9           #0xE1E3E9
+export WARM_GRAY=0xFF9CA0B0       #9CA0B0
+export DARK_INK=0xFF1F2133        #0x1F2133
 
-export ITEM_COLOR=0xff1e1e2e #1e1e2e
-# export ITEM_COLOR=$BLACK
+#################### Bar Colors ######################
+export BAR_COLOR=$SHADOW_GRAY
+export ICON_COLOR=$WHITE
+export LABEL_COLOR=$WHITE
 
-# General bar colors
-export BAR_COLOR=$DARK_WHITE_SEMI_TRANSPARENT
-export ICON_COLOR=$BRIGHT_WHITE # Color of all icons
-export LABEL_COLOR=$BRIGHT_WHITE # Color of all labels
-export ALT_LABEL_COLOR=$WHITE
-export FRONT_APP_COLOR=$BRIGHT_WHITE
-
-export OPEN_APPS_BACKGROUND=$ITEM_COLOR
-# export CALENDAR_BACKGROUND=0xffb8c0e0
-
-# export POPUP_BACKGROUND_COLOR=$BLACK
-# export POPUP_BORDER_COLOR=$WHITE
-
-# export SHADOW_COLOR=$BLACK
-
+#################### Front App Colors ################
+export FRONT_APP_COLOR=$WHITE
+export WINDOW_ICON_SELECTED=$MIDNIGHT_BLUE
+export WINDOW_LABEL_SELECTED=$MIDNIGHT_BLUE
 
 #################### Space Colors ####################
-export SPACE_BACKGROUND=$ITEM_COLOR
-#export SPACE_BACKGROUND2=0xffa6adc8
-export SPACE_SELECTED=$PEACH
-# export SPACE_DESELECTED=$LAVENDER
-# export SPACE_BACKGROUND2=0xffa6adc8
-#export SPACE_SELECTED=0xffcdd6f4
-export SPACE_DESELECTED=0xff313244
+export SPACE_SELECTED=$ORANGE
+export SPACE_ICON_SELECTED=$DARK_INK
+export SPACE_LABEL_SELECTED=$DARK_INK
 
-export SPACE_ICON_SELECTED=$BLACK
-export SPACE_ICON_DESELECTED=$WHITE
-
-export SPACE_LABEL_SELECTED=$BLACK
-export SPACE_LABEL_DESELECTED=$WHITE
-
-export WINDOW_ICON_SELECTED=$NAVY
-export WINDOW_LABEL_SELECTED=$NAVY
-
-
+export SPACE_DESELECTED=$MIDNIGHT_BLUE
+export SPACE_ICON_DESELECTED=$WARM_GRAY
+export SPACE_LABEL_DESELECTED=$WARM_GRAY
 
 #################### Windows Colors ####################
-export MONITOR_1_BACKGROUND=$PEACH
-export MONITOR_1_LABEL=$NAVY
-export MONITOR_1_ICON=$NAVY
+export MONITOR_1_BACKGROUND=$ORANGE
+export MONITOR_1_ICON=$DARK_INK
+export MONITOR_1_LABEL=$DARK_INK
 
-export MONITOR_2_BACKGROUND=$NAVY
-export MONITOR_2_LABEL=$PEACH
-export MONITOR_2_ICON=$PEACH
+export MONITOR_2_BACKGROUND=$NAVY_BLUE
+export MONITOR_2_ICON=$WHITE
+export MONITOR_2_LABEL=$WHITE
+
+export WINDOW_ITEM_BACKGROUND=$MIDNIGHT_BLUE
+export WINDOW_ITEM_ICON=$WARM_GRAY
+export WINDOW_ITEM_LABEL=$WARM_GRAY
