db12783bea55c4d56636ee4b421f41fbdacd44a1
Apurva Jantrania <apurvaja@amazon.com>
2025-02-25 11:30:02 -0800

    Fix: Only disable icon drawing for headphones to allow script to detect when headphones are turned on

--- sketchybar/plugins/headphones.sh
+++ sketchybar/plugins/headphones.sh
@@ -3,7 +3,7 @@
 CURRENT_DEVICE=$(SwitchAudioSource -c)
 
 if [[ $CURRENT_DEVICE == "WH-1000XM5" ]]; then
-  sketchybar --set $NAME drawing=on
+  sketchybar --set $NAME icon.drawing=on
 else
-  sketchybar --set $NAME drawing=off
+  sketchybar --set $NAME icon.drawing=off
 fi
