f73a5c75996cd23062b765dc4a5a39dc9a3fab32
Apurva Jantrania <jmareel@gmail.com>
2025-10-15 10:32:59 +0900

    Fix restart k3d alias

--- zsh/archodex.zsh
+++ zsh/archodex.zsh
@@ -2,4 +2,4 @@ export AWS_PROFILE=archodex-apurva-AdministratorAccess
 export AWS_SDK_LOAD_CONFIG=1
 export AWS_REGION=us-west-2
 
-export RESTART_K3D='cd /Users/ajantrania/code/archodex/microservices-demo && k3d cluster stop archodex-dev && k3d cluster start archodex-dev && VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" cluster-keys.json) && sleep 5 && kubectl exec --namespace vault vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY && cd --'
+alias RESTART_K3D='cd /Users/ajantrania/code/archodex/microservices-demo && k3d cluster stop archodex-dev && sleep 5 && k3d cluster start archodex-dev && VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" cluster-keys.json) && sleep 20 && kubectl exec --namespace vault vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY && cd --'
