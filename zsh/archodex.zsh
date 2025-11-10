0233f919aa8477a9c8dffabd8b781a093f9a186c
Apurva Jantrania <jmareel@gmail.com>
2025-11-10 14:45:41 +0900

    zsh: Archodex - Extract Unseal Valut to its own alias

--- zsh/archodex.zsh
+++ zsh/archodex.zsh
@@ -2,4 +2,5 @@ export AWS_PROFILE=archodex-apurva-AdministratorAccess
 export AWS_SDK_LOAD_CONFIG=1
 export AWS_REGION=us-west-2
 
-alias RESTART_K3D='cd /Users/ajantrania/code/archodex/microservices-demo && k3d cluster stop archodex-dev && sleep 5 && k3d cluster start archodex-dev && VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" cluster-keys.json) && sleep 20 && kubectl exec --namespace vault vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY && cd --'
+alias UNSEAL_VAULT_MICROSERVICES_DEMO='VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" /Users/ajantrania/code/archodex/microservices-demo/cluster-keys.json) && echo "waiting 20 seconds..." && sleep 20 && kubectl exec --namespace vault vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY'
+alias RESTART_K3D='cd /Users/ajantrania/code/archodex/microservices-demo && k3d cluster stop archodex-dev && sleep 5 && k3d cluster start archodex-dev && UNSEAL_VAULT_MICROSERVICES_DEMO && cd --'
